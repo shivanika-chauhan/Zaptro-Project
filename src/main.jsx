5d427df178340f63045531e89a73d43b2c633e45
shivanika-chauhan <shivanikachauhan813@gmail.com>
2025-08-31 10:27:12 +0530

    Upload project code

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
-}cddcd
+}
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
